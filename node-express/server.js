f9e884dc80ba58dba195f36cf7197a82ceada835
Ena Jenkins <enajenkins@gmail.com>
2020-11-20 21:05:17 -0600

    express - simple REST

--- node-express/server.js
+++ node-express/server.js
@@ -10,6 +10,50 @@ const app = express(); // returns an express server application that is avaiable
 
 // adding Morgan middleware
 app.use(morgan('dev'));
+app.use(express.json());
+
+// Set up a REST API 
+app.all('/campsites', (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();
+});
+
+app.get('/campsites', (req, res) => {
+  res.end('Will send all the campsites to you');
+});
+
+app.post('/campsites', (req, res) => {
+  res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+  res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId', (req, res) => {
+  res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+  res.statusCode = 403;
+  res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+  res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+  res.end(`Will update the campsite: ${req.body.name}
+      with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+  res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
 
 // set up express to serve files from the public folder
 // __dirname will refer to the absolute path of the current containing directory 
