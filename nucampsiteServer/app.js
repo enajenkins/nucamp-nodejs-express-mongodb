fa62b4c785d264b59ee28f83714a87123553615a
Ena Jenkins <enajenkins@gmail.com>
2020-12-09 19:24:53 -0600

    express session pt 1

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -34,11 +36,6 @@ connect.then(() => console.log('Connected correctly to server'),
     err => console.log(err)
 );
 
-
-
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -48,12 +45,54 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 // custom middleware function. it must have the req and res objects as params. this is true of all Express middleware functions. next is optional but good practice
 
+// function auth(req, res, next) {
+//   if (!req.signedCookies.user) {
+//       const authHeader = req.headers.authorization;
+//       if (!authHeader) {
+//           const err = new Error('You are not authenticated!');
+//           res.setHeader('WWW-Authenticate', 'Basic');
+//           err.status = 401;
+//           return next(err);
+//       }
+
+//       const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+//       const user = auth[0];
+//       const pass = auth[1];
+//       if (user === 'admin' && pass === 'password') {
+//           res.cookie('user', 'admin', {signed: true});
+//           return next(); // authorized
+//       } else {
+//           const err = new Error('You are not authenticated!');
+//           res.setHeader('WWW-Authenticate', 'Basic');
+//           err.status = 401;
+//           return next(err);
+//       }
+//   } else {
+//       if (req.signedCookies.user === 'admin') {
+//           return next();
+//       } else {
+//           const err = new Error('You are not authenticated!');
+//           err.status = 401;
+//           return next(err);
+//       }
+//   }
+// }
+
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
+  console.log(req.session);
+
+  if (!req.session.user) {
       const authHeader = req.headers.authorization;
       if (!authHeader) {
           const err = new Error('You are not authenticated!');
@@ -66,7 +105,7 @@ function auth(req, res, next) {
       const user = auth[0];
       const pass = auth[1];
       if (user === 'admin' && pass === 'password') {
-          res.cookie('user', 'admin', {signed: true});
+          req.session.user = 'admin';
           return next(); // authorized
       } else {
           const err = new Error('You are not authenticated!');
@@ -75,7 +114,7 @@ function auth(req, res, next) {
           return next(err);
       }
   } else {
-      if (req.signedCookies.user === 'admin') {
+      if (req.session.user === 'admin') {
           return next();
       } else {
           const err = new Error('You are not authenticated!');
